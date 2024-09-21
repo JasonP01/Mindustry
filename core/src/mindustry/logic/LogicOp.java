eae42cef947e7fde7788fffa1570f1a7ae523d3c
Jason <131086642+JasonP01@users.noreply.github.com>
2024-09-21 19:27:06 +0200

    Bruh
    Signed-off-by: Jason <131086642+JasonP01@users.noreply.github.com>

--- core/src/mindustry/logic/LogicOp.java
+++ core/src/mindustry/logic/LogicOp.java
@@ -40,7 +40,7 @@ public enum LogicOp{
     log10("log10", Math::log10),
     floor("floor", Math::floor),
     ceil("ceil", Math::ceil),
-    round("round:, Math::round),
+    round("round", Math::round),
     sqrt("sqrt", Math::sqrt),
     rand("rand", d -> GlobalVars.rand.nextDouble() * d),
 
